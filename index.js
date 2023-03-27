222ce3eed84092d2097c89f5984967722f8dded5
Samarth Saxena <samarthsaxena1672003@gmail.com>
2023-03-27 14:57:37 +0530

    checking

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ app.get("/api/users/:id/logs", (req, res) => {
   });
 });
 
-const listener = app.listen(3000, () => {
+const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
 //
